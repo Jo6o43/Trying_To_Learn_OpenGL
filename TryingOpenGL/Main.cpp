233a7db704264d16d2a2089ef2c54a4dd913e39b
Jo6o43 <joaoribeiro141516@gmail.com>
2024-12-16 22:25:10 +0000

    Shaders Organizing

--- TryingOpenGL/Main.cpp
+++ TryingOpenGL/Main.cpp
@@ -2,20 +2,6 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-//vertex shaders source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position= vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\n";
-//fragment shaders source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";
 
 int main()
 {
